fb4de7f26dae982d5497355a90267d29e667b608
unknown <tomasmiranda98@hotmail.com>
2021-10-29 13:56:22 -0300

    se agrega funcionalidad de mensajes y libreria mui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 function App() {
+  //input es la entrada del state, setInput es la q modifca el state
+  const [input,setInput] =  useState('');
+  //en este state se almacenan los mensajes enviados
+  const [messages,setMessage] = useState([]);
+  
+  console.log(input);
+  console.log(messages);
+
+  const functionSendMessage = (event) => {
+    event.preventDefault()
+    //se agrega input sin perder lo q habia hasta el momento en el state message(q es un array)  
+    setMessage([...messages,input])
+    //se limpia el input
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Hola Perris</h1>
+      
+      <form>
+          <input value /*muestra el valor inicial*/={input}/*en este caso el state*/ onChange = {event => setInput(event.target.value)} /*{event} no porq le tengo q pasar la funcion exacta*/ ></input>
+          <Button variant="contained" type = 'submit' onClick = {functionSendMessage} >Enviar Mensaje</Button>
+      </form>
+      {}
+      {
+        //se recorre el state de message y se muestra su contenido
+        messages.map(message =>(
+          <p>{message}</p>
+        ))
+      }
+    
     </div>
   );
 }
